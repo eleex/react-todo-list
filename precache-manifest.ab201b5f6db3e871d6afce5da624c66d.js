e3049c69d303f1038f854c1730d38a364ba4ec4b
eleex <eleex1988@gmail.com>
2020-04-11 23:33:07 +0300

    Updates

---
+++ precache-manifest.ab201b5f6db3e871d6afce5da624c66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce698494ae0ff1a2160cd0f8e482cb9",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "dbaf986b9ed843aa43fd",
+    "url": "/react-todo-list/static/css/main.8a2b412c.chunk.css"
+  },
+  {
+    "revision": "532a3c6b7a2b83751e6b",
+    "url": "/react-todo-list/static/js/2.8e9e2458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-list/static/js/2.8e9e2458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbaf986b9ed843aa43fd",
+    "url": "/react-todo-list/static/js/main.5bbd8988.chunk.js"
+  },
+  {
+    "revision": "0c826ca761240eed1260",
+    "url": "/react-todo-list/static/js/runtime-main.02781736.js"
+  }
+]);
